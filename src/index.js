4faa5829ebfd275bc5cc6aa0e0914d21b11ffdee
Toni Torredemer <toni.torredemer.argiles@gmail.com>
2023-04-24 01:01:50 +0200

    dificil

--- src/index.js
+++ src/index.js
@@ -4,17 +4,52 @@ function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
   //... your code goes here
+
+  const price = product.querySelector('.price span');
+  /* console.log("price", price);  */
+
+  const quantity = product.querySelector(".quantity input")
+  /* console.log("quantity", quantity); */
+  
+  const priceValue = price.innerHTML;
+/*   console.log("price value:", priceValue); */ 
+
+  const quantityValue = quantity.value; 
+/*   console.log("quantity value", quantityValue); */ 
+
+  const result = quantityValue * priceValue; 
+  /* console.log("Total:", result);  */
+
+  const subtotal = product.querySelector('.subtotal span');
+/*   console.log("subtotal:", subtotal) */
+
+  subtotal.innerHTML = result; 
+
+  return subtotal.innerHTML; 
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  
+    const products = document.getElementsByClassName('product');
+    
+
+    for (let i = 0; i < products.length; i++) {
+      const productRow = products[i];
+      
+      updateSubtotal(productRow);
+    }
+  }
+  
+
+
+
+
+
+
 
   // ITERATION 2
-  //... your code goes here
+
+
 
   // ITERATION 3
   //... your code goes here
